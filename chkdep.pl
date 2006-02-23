f1821b1fc68d9bed087467e3af073bf7c1d285d3
VMiklos <vmiklos@frugalware.org>
2006-02-23 21:02:11 +0000

    UNDO: chkdep: drop xorg->x, add mesa->opengl

--- chkdep.pl
+++ chkdep.pl
@@ -75,7 +75,7 @@ use Data::Dumper;
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 our $VERSION = "1.9";
 
-my %alias = ( 'mesa' => 'opengl' );
+my %alias = ( 'xorg' => 'x' );
 my %reversalias = map { $alias{$_} => $_ } keys %alias;
 
 my %opts;
