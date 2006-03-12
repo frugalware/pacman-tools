d1fbf3d2aa4ba330a3154c1c58d2225a5ea2268c
VMiklos <vmiklos@frugalware.org>
2006-03-12 20:34:24 +0000

    chkdep: opengl provides libgl

--- chkdep.pl
+++ chkdep.pl
@@ -75,7 +75,8 @@ use Data::Dumper;
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
 our $VERSION = "1.9";
 
-my %alias = ( 'xorg' => 'x' );
+my %alias = ( 'xorg' => 'x',
+	      'libgl' => 'opengl');
 my %reversalias = map { $alias{$_} => $_ } keys %alias;
 
 my %opts;
