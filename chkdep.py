5d244c640e5fa3fae5258dca195bb84df613a5ac
VMiklos <vmiklos@frugalware.org>
2007-04-09 21:19:22 +0000

    chkdep: added some optimization - call ldd as less time as possible

--- chkdep.py
+++ chkdep.py
@@ -17,6 +17,8 @@ You should at least use the -d or the -p option.
 
 import tarfile, tempfile, shutil, os, stat, re, pacman, getopt, sys
 
+from modulefinder import ModuleFinder
+
 def usage():
 	print __doc__
 
@@ -40,7 +42,30 @@ def rmdupdeps(deps):
 			newdeps.append(i)
 	return newdeps
 
+def detect_owner(lib):
+	#print "[DEBUG] detect_owner(%s)" % lib
+	if lib.find(fpmroot) == 0:
+		return
+	pkg = pacman.void_to_PM_PKG(pacman.list_getdata(pacman.pkg_getowners(lib)))
+	owner = pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_NAME))
+	if not owner:
+		print "WARNING: No package found containing %s!" % lib
+	elif owner not in deps:
+		deps.append(owner)
+
 class Checks:
+	def python(self, file):
+		mf = ModuleFinder()
+		try:
+			mf.run_script(file)
+		except:
+			return
+
+		for key in mf.modules.keys():
+			file = mf.modules[key].__file__
+			if file:
+				detect_owner(file)
+
 	def elf(self, file):
 		if not os.stat(file)[stat.ST_MODE] & stat.S_IXUSR:
 			return
@@ -50,12 +75,7 @@ class Checks:
 				continue
 			lib = re.sub(r".* => (.*) \(.*", r"\1", i.strip())
 			if len(lib):
-				pkg = pacman.void_to_PM_PKG(pacman.list_getdata(pacman.pkg_getowners(lib)))
-				owner = pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_NAME))
-				if not owner:
-					print "WARNING: No package found containing %s!" % lib
-				elif owner not in deps:
-					deps.append(owner)
+				detect_owner(lib)
 
 checks = Checks()
 
@@ -103,12 +123,19 @@ if pkg:
 if dir:
 	fpmroot = dir
 
+depfiles = []
 for root, dirs, files in os.walk(fpmroot):
 	for file in files:
-		checker(os.path.join(root, file))
+		ret = checker(os.path.join(root, file))
+		if ret:
+			for i in checker(os.path.join(root, file)):
+				if i not in depfiles:
+					depfiles.append(i)
+for i in depfiles:
+	detect_owner(i)
 
 if pkg:
 	shutil.rmtree(fpmroot)
 
 deps = rmdupdeps(deps)
-print deps
+print "depends=('" + "' '".join(deps) + "')"
