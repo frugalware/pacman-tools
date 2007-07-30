849b22e8f6a748ee390ec935c04ca83cb88985ca
VMiklos <vmiklos@frugalware.org>
2007-07-30 22:07:26 +0200

    update the client accordingly

--- syncpkgc.py
+++ syncpkgc.py
@@ -1,6 +1,6 @@
 import xmlrpclib
 syncd = xmlrpclib.Server("http://localhost:1873")
 
-print syncd.request_build("user", "pass", "pkg", "i686")
-print syncd.request_build("user", "pass", "pkg", "x86_64")
+print syncd.request_build("user", "pass", "pkg-1.0-1-i686")
+print syncd.request_build("user", "pass", "pkg-1.0-1-x86_64")
 print syncd.get_todo("user", "pass")
