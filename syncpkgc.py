76c63fdf7415f91349ef8c325c1ea0b951ed5fd6
VMiklos <vmiklos@frugalware.org>
2007-08-04 01:31:35 +0000

    client: more samples

--- syncpkgc.py
+++ syncpkgc.py
@@ -3,4 +3,5 @@ syncd = xmlrpclib.Server("http://localhost:1873")
 
 print syncd.request_build("user", "pass", "pkg-1.0-1-i686")
 print syncd.request_build("user", "pass", "pkg-1.0-1-x86_64")
-print syncd.get_todo("user", "pass")
+#print syncd.get_todo("user", "pass")
+#print syncd.request_pkg("user", "pass", "i686")
