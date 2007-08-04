73ad7a18645c1f0e73c4d4ffa7a6fede83d58fea
VMiklos <vmiklos@frugalware.org>
2007-08-04 01:36:15 +0000

    client: disable everything by default

--- syncpkgc.py
+++ syncpkgc.py
@@ -1,7 +1,7 @@
 import xmlrpclib
 syncd = xmlrpclib.Server("http://localhost:1873")
 
-print syncd.request_build("user", "pass", "pkg-1.0-1-i686")
-print syncd.request_build("user", "pass", "pkg-1.0-1-x86_64")
+#syncd.request_build("user", "pass", "pkg-1.0-1-i686")
+#syncd.request_build("user", "pass", "pkg-1.0-1-x86_64")
 #print syncd.get_todo("user", "pass")
 #print syncd.request_pkg("user", "pass", "i686")
