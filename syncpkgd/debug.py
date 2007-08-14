ebd0f70b39f8063432bb726a948e348bc174880f
VMiklos <vmiklos@frugalware.org>
2007-08-14 04:38:05 +0200

    remove leftover debug.py

--- syncpkgd/debug.py
+++
@@ -1,8 +0,0 @@
-import xmlrpclib, time, os, base64
-
-server_url = "http://frugalware.org/~vmiklos/syncd2/"
-server_user = "user"
-server_pass = "pass"
-server = xmlrpclib.Server(server_url)
-
-print server.request_build(server_user, server_pass, "git://current/frugalware-0.7pre2-1-i686")
