d92623f34ae78bdf51ea63761dab6f0f753416bd
VMiklos <vmiklos@frugalware.org>
2007-10-01 19:33:47 +0200

    added syncpkgdctl manpage

--- syncpkgd/syncpkgdctl.py
+++ syncpkgd/syncpkgdctl.py
@@ -12,11 +12,7 @@ if len(sys.argv) > 1:
 	else:
 		print "Oops, something went wrong. Maybe this package is already in the queue?"
 else:
-	print """Hello, this is the syncpkgd control client.
-If you want to add a new package, just pass its url as a parameter, ie.:
-syncpkgdctl 'git://current/frugalwareutils-0.7.4-2-x86_64/VMiklos <vmiklos@frugalware.org>'
-(The address is used to notify you in case the build fails. Please use
-your address only.)
-At the moment the following packages are waiting to be built:"""
+	print """At the moment the following packages are waiting to be built:"""
 	print server.get_todo(config.server_user, config.server_pass)
 	print "Please not that this list does not include failed or already started builds."
+	print "See man syncpkgdctl for more info."
